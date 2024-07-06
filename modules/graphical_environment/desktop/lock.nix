c31381df3f1a6bfc4b733a4457dd4cf36bcb13ba
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-07-06 20:09:03 +0200

    [ Sat Jul  6 08:09:03 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/graphical_environment/desktop/lock.nix
+++ modules/graphical_environment/desktop/lock.nix
@@ -176,12 +176,12 @@
         #!/run/current-system/sw/bin/bash
         export XDG_RUNTIME_DIR="/run/user/1000;  #$(loginctl list-sessions | ${pkgs.gawk}/bin/awk 'NR==2 {print $2}')";
         #loginctl lock-session $session_id && sleep 1
-        ${pkgs.hyprlock}/bin/hyprlock |
-        while read -r line; do
-          if [[ $line == "[LOG] onLockLocked called" ]]; then 
-            break
-          fi
-        done
+        ${pkgs.hyprlock}/bin/hyprlock || > ded && > noded
+        #while read -r line; do
+        #  if [[ $line == "[LOG] onLockLocked called" ]]; then 
+        #    break
+        #  fi
+        #done
       ''}/bin/lockBeforeSleep";
     };
   };
