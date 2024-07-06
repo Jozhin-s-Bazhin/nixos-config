cfbcbb8c6b6aff366fd471a11b33dc793c036d5b
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-07-06 20:00:35 +0200

    [ Sat Jul  6 08:00:35 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/graphical_environment/desktop/lock.nix
+++ modules/graphical_environment/desktop/lock.nix
@@ -174,8 +174,7 @@
       Environment = "DISPLAY=:0";
       ExecStart = "${pkgs.writeScriptBin "lockBeforeSleep" ''
         #!/run/current-system/sw/bin/bash
-        XDG_RUNTIME_DIR="/run/user/$(loginctl list-sessions | ${pkgs.gawk}/bin/awk 'NR==2 {print $2}')";
-	echo $XDG_RUNTIME_DIR;
+        export XDG_RUNTIME_DIR="/run/user/$(loginctl list-sessions | ${pkgs.gawk}/bin/awk 'NR==2 {print $2}')";
         #loginctl lock-session $session_id && sleep 1
         ${pkgs.hyprlock}/bin/hyprlock |
         while read -r line; do
