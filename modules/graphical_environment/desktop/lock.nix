0c06ea10ca1c7511a0367e660f3f4fc25f76d6b1
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-07-06 18:30:05 +0200

    [ Sat Jul  6 06:30:05 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/graphical_environment/desktop/lock.nix
+++ modules/graphical_environment/desktop/lock.nix
@@ -181,7 +181,7 @@
     before = [ "sleep.target" ];
     wantedBy = [ "sleep.target" ];
     serviceConfig = {
-      Type = "exec";
+      Type = "simple";
       User = username;
       Environment = "DISPLAY=:0";
       ExecStart = "${pkgs.writeScriptBin "my-sleep-script" ''
