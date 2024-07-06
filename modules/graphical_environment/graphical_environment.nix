df126a8cf5897791896ca127ad2ec7156ea30ed6
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-07-06 13:53:05 +0200

    [ Sat Jul  6 01:53:05 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/graphical_environment/graphical_environment.nix
+++ modules/graphical_environment/graphical_environment.nix
@@ -7,6 +7,5 @@
     ./appconfig.nix  # A config for all of my apps unrelated to the desktop
     ./desktop/hyprland.nix  # Hyprland and all related things
     ./ags/ags.nix
-    ./junk.nix  # Junk that I should (re)move/refactor
   ];
 }
