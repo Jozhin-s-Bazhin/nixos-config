bec438dd18568f9aee8f13a564abfd6f7115380a
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-07-06 18:52:38 +0200

    [ Sat Jul  6 06:52:38 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/graphical_environment/desktop/lock.nix
+++ modules/graphical_environment/desktop/lock.nix
@@ -103,25 +103,23 @@
             valign = "center";
           }
         ];
-        input_field = [
+        input-field = [
           {
             monitor = "";
             size = "200, 50";
-            outline_thickness = 3;
+            outline_thickness = 0;
             dots_size = 0.33; # Scale of input-field height, 0.2 - 0.8
             dots_spacing = 0.15; # Scale of dots' absolute size, 0.0 - 1.0
-            dots_center = false;
+            dots_center = true;
             dots_rounding = -1; # -1 default circle, -2 follow input-field rounding
-            outer_color = "rgb(151515)";
             inner_color = "rgb(200, 200, 200)";
-            font_color = "rgb(10, 10, 10)";
-            fade_on_empty = true;
-            fade_timeout = 1000; # Milliseconds before fade_on_empty is triggered.
-            placeholder_text = "<i>Input Password</i>"; # Text rendered in the input box when it's empty.
+            font_color = "rgb(25, 25, 25)";
+            fade_on_empty = false;
+            placeholder_text = "<i>Enter password</i>"; # Text rendered in the input box when it's empty.
             hide_input = false;
             rounding = -1; # -1 means complete rounding (circle/oval)
-            check_color = "rgb(204, 136, 34)";
-            fail_color = "rgb(204, 34, 34)";  # if authentication failed, changes outer_color and fail message color
+            check_color = "rgb(200, 200, 200)";
+            fail_color = "rgb(200, 200, 200)";  # if authentication failed, changes outer_color and fail message color
             fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>"; # can be set to empty
             fail_transition = 300; # transition time in ms between normal outer_color and fail_color
             capslock_color = -1;
