98cc83b401faa0473f532ce775cdd044c28a8c88
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-07-08 14:09:04 +0200

    [ Mon Jul  8 02:09:04 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/graphical_environment/theming.nix
+++ modules/graphical_environment/theming.nix
@@ -5,14 +5,46 @@
     #inputs.stylix.homeManagerModules.stylix
   ];
   
-  home-manager.users.${username}.stylix = {
-    image = "${configDir}/modules/graphical_environment/wallpaper/wallpaper.jpg";
+  /*home-manager.users.${username}.stylix = {
     enable = true;
-    /*cursor = {
+    image = "${configDir}/modules/graphical_environment/wallpaper/wallpaper.jpg";
+    cursor = {
       name = "Bibata";
       package = pkgs.bibata-cursors;
       size = 128;
     };
-    polarity = "dark";*/
+    polarity = "dark";
+  };*/
+home-manager.users.${username}.stylix = {
+    enable = true;
+
+    image = pkgs.fetchurl {
+      url = "https://cdnb.artstation.com/p/assets/images/images/016/252/301/4k/grady-frederick-atlantis-garbageman-v2.jpg";
+      sha256 = "tAX6qTm1/7v/auvCHrmRswJsScNieSWpXV6TCBhRP7Y=";
+    };
+
+    polarity = "dark";
+
+    fonts = {
+      serif = {
+        name = "Cantarell";
+        package = pkgs.cantarell-fonts;
+      };
+
+      sansSerif = {
+        name = "Cantarell";
+        package = pkgs.cantarell-fonts;
+      };
+
+      monospace = {
+        name = "Fira Code";
+        package = pkgs.fira-code;
+      };
+
+      sizes = {
+        applications = 11;
+        desktop = 11;
+      };
+    };
   };
 }
