caeadca375cbfb266fbe1ff2633af3c431fe7fe4
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-07-08 16:23:13 +0200

    [ Mon Jul  8 04:23:13 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/graphical_environment/desktop/lock.nix
+++ modules/graphical_environment/desktop/lock.nix
@@ -184,8 +184,10 @@
       User = username;
       ExecStart = "${pkgs.writeScriptBin "lockBeforeSleep" ''
         #!/run/current-system/sw/bin/bash
+
         export XDG_RUNTIME_DIR="/run/user/$(loginctl list-sessions | ${pkgs.gawk}/bin/awk 'NR==2 {print $2}')";
         export WAYLAND_DISPLAY="wayland-$(loginctl list-sessions | ${pkgs.gawk}/bin/awk 'NR==2 {print $1}')";
+
         ${pkgs.hyprlock}/bin/hyprlock |
         while read -r line; do
           if [[ $line == "[LOG] onLockLocked called" ]]; then 
