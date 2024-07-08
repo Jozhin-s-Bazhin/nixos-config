66dd7baf7d6832676fb864bb2552d583697eaa49
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-07-08 22:24:12 +0200

    [ Mon Jul  8 10:24:12 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/graphical_environment/desktop/lock.nix
+++ modules/graphical_environment/desktop/lock.nix
@@ -197,10 +197,10 @@ logger () {
 }
 
 logger "starting hyprlock"
-#${pkgs.hyprlock}/bin/hyprlock 2>&1 >/dev/null | while read -r line; do
+#${pkgs.hyprlock}/bin/hyprlock 2>&1 >/dev/null | while read line; do
 ${pkgs.util-linux}/bin/script -f -c "${pkgs.hyprlock}/bin/hyprlock 2>/dev/null" /dev/null | while read -r line; do
 logger "got line '$line'"
-if [[ $line == "[LOG] PAM_PROMPT: Password:"* ]]; then
+if [[ $line == "[LOG] PAM_PROMPT: Password: " ]]; then
 	systemd-notify --ready
 	logger "systemd done"
 fi
