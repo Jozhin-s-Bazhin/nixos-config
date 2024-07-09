d50e54f536620d7c6db13b4f6f95ce3318ce8f30
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-07-09 22:57:33 +0200

    [ Tue Jul  9 10:57:33 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/graphical_environment/desktop/lock.nix
+++ modules/graphical_environment/desktop/lock.nix
@@ -191,29 +191,29 @@
 export XDG_RUNTIME_DIR="/run/user/$(loginctl list-sessions | ${pkgs.gawk}/bin/awk 'NR==2 {print $2}')"
 export WAYLAND_DISPLAY="wayland-$(loginctl list-sessions | ${pkgs.gawk}/bin/awk 'NR==2 {print $1}')"
 
-logger () {
-  #echo $1 | ${pkgs.systemd}/bin/systemd-cat -t hyprlock
-  echo $1 > /dev/ttyUSB0
-}
+#logger () {
+#  #echo $1 | ${pkgs.systemd}/bin/systemd-cat -t hyprlock
+#  echo $1 > /dev/ttyUSB0
+#}
 
-logger "starting hyprlock"
+#logger "starting hyprlock"
 
 #${pkgs.hyprlock}/bin/hyprlock 2>&1 >/dev/null | while read -r line; do
 #${pkgs.util-linux}/bin/script -f -c "${pkgs.hyprlock}/bin/hyprlock 2>/dev/null" /dev/null | while read -r line; do
 
 ${pkgs.util-linux}/bin/script -f -c "${pkgs.hyprlock}/bin/hyprlock 2>/dev/null" /dev/null | while IFS= read -r line ; do
 
-logger "got line xx '$line'"
+#logger "got line xx '$line'"
 #if [[ $line == " [LOG] PAM_PROMPT: Password: " ]]; then
 regexp=".*PAM.*"
 if [[ $line =~ $regexp ]]; then
-	logger "systemd x"
+	#logger "systemd x"
 	systemd-notify --ready
-	logger "systemd done"
+	#logger "systemd done"
 	break
 fi
 done
-logger "finished"
+#logger "finished"
       ''}/bin/findme";
     };
   };
