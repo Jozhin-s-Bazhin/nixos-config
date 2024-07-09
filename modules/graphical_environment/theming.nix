5757e2289a03dce935c9730bd05656ad2b4c36bd
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-07-09 23:52:46 +0200

    [ Tue Jul  9 11:52:46 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/graphical_environment/theming.nix
+++ modules/graphical_environment/theming.nix
@@ -4,16 +4,18 @@
     inputs.stylix.nixosModules.stylix
   ];
   
-  #stylix.enable = true;
-  #stylix.image = "${configDir}/modules/graphical_environment/wallpaper/wallpaper.png";
-  home-manager.users.${username}.stylix = {
+  stylix = {
     enable = true;
     image = "${configDir}/modules/graphical_environment/wallpaper/wallpaper.png";
-    /*cursor = {
+  };
+  /*home-manager.users.${username}.stylix = {
+    enable = true;
+    image = "${configDir}/modules/graphical_environment/wallpaper/wallpaper.png";
+    cursor = {
       name = "Bibata";
       package = pkgs.bibata-cursors;
       size = 128;
-    };*/
+    };
     #polarity = "dark";
-  };
+  };*/
 }
