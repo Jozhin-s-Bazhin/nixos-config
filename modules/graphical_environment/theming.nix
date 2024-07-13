c56e7412adb6b68fe27df36244b56d77ecb166d6
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-07-13 23:01:45 +0200

    [ Sat Jul 13 11:01:45 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/graphical_environment/theming.nix
+++ modules/graphical_environment/theming.nix
@@ -5,6 +5,7 @@
   ];
   
   home-manager.users.${username}.stylix = {
+    enable = true;
     image = "${configDir}/modules/graphical_environment/wallpaper/wallpaper.png";
     cursor = {
       name = "Bibata";
