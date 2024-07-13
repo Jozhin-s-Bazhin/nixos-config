d200d36e91baa76bc77fc60fc2633194815d42cc
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-07-13 16:40:38 +0200

    [ Sat Jul 13 04:40:38 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -9,7 +9,7 @@
   
   # Steam
   programs.steam = {
-    #enable = true;
+    enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   };
