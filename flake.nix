e9e39df82764dca6ae39d391aee13d305bbed9c4
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-07-13 22:56:35 +0200

    [ Sat Jul 13 10:56:35 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-    stylix.url = "github:danth/stylix/0fc4e9f1449a9dce4be7a1ecedd97949da591181";  # Old version because bug
+    stylix.url = "github:danth/stylix/1ddb78df07579a1cd9d7cd3a98de08137cd80863";  # Old version because bug
     ags.url = "github:Aylur/ags";
   };
 
