8c3713977a207eec9def961375a25448afc68606
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-07-15 20:37:05 +0200

    [ Mon Jul 15 08:37:05 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/amdgpu.nix
+++ modules/amdgpu.nix
@@ -3,13 +3,14 @@
 {
   hardware.graphics = {
     enable = true;
-    extraPackages = with pkgs; [ 
+    enable32Bit = true;
+    /*extraPackages = with pkgs; [ 
       #amdvlk 
       rocmPackages.clr.icd
     ];
     extraPackages32 = [
       #pkgs.driversi686Linux.amdvlk
-    ];
+    ];*/
   };
   #environment.variables.VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json";  # Set RADV as default
   environment.systemPackages = [ pkgs.radeontop ];
