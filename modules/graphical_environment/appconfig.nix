e4d79b5b3728500e46ff943a191fdc0ddedfeaf5
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-07-16 23:35:35 +0200

    [ Tue Jul 16 11:35:35 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/graphical_environment/appconfig.nix
+++ modules/graphical_environment/appconfig.nix
@@ -10,7 +10,7 @@
         #background_opacity = toString colors.opacity;
         confirm_os_window_close = "0";
         window_padding_width = "3";
-        font_size = "13.0";
+        font_size = "12.0";
         enable_audio_bell = "no";
         hide_window_decorations = "yes";
 	linux_display_server = "wayland";
