2734025e2bf4b61770b66bea12efd4ddb3e055a5
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-07-16 22:37:31 +0200

    [ Tue Jul 16 10:37:31 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/graphical_environment/theming.nix
+++ modules/graphical_environment/theming.nix
@@ -2,19 +2,25 @@
 {
   imports = [ inputs.stylix.nixosModules.stylix ];
   
+  # This doesn't work properly. 
   stylix = {
     enable = true;
-    image = ./wallpaper/wallpaper.png;
+    image = ./wallpaper/wallpaper.jpg;
     cursor = {
       name = "Bibata";
       package = pkgs.bibata-cursors;
-      size = 128;
+      size = 24;
     };
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/classic-dark.yaml";
   };
   home-manager.users.${username}.stylix = {
     enable = true;
-    #image = "./wallpaper/wallpaper.png";
-
-    polarity = "dark";
+    image = ./wallpaper/wallpaper.jpg;
+    cursor = {
+      name = "Bibata";
+      package = pkgs.bibata-cursors;
+      size = 24;
+    };
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/classic-dark.yaml";
   };
 }
