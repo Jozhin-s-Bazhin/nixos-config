2e592ee5783f295f1f514894d15043d4091dba68
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-07-16 14:45:30 +0200

    [ Tue Jul 16 02:45:30 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/amdgpu.nix
+++ modules/amdgpu.nix
@@ -5,12 +5,12 @@
     enable = true;
     enable32Bit = true;
     extraPackages = with pkgs; [ 
-      #amdvlk 
+      amdvlk 
       rocmPackages.clr.icd
     ];
-    /*extraPackages32 = [
+    extraPackages32 = [
       pkgs.driversi686Linux.amdvlk
-    ];*/
+    ];
   };
   #environment.variables.AMD_VULKAN_ICD = "RADV";
   environment.systemPackages = [ pkgs.radeontop ];
