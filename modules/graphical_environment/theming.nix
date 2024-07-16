4976b2ba4b23f719ebf9c7acfce3cb79164aa884
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-07-16 22:46:27 +0200

    [ Tue Jul 16 10:46:27 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/graphical_environment/theming.nix
+++ modules/graphical_environment/theming.nix
@@ -2,25 +2,20 @@
 {
   imports = [ inputs.stylix.nixosModules.stylix ];
   
-  # This doesn't work properly. 
-  stylix = {
-    #enable = true;
-    image = ./wallpaper/wallpaper.jpg;
-    /*cursor = {
-      name = "Adwaita";
-      package = pkgs.gnome.adwaita-icon-theme;
-      size = 24;
+  stylix.image = ./wallpaper/wallpaper.jpg;  # It breaks without this
+  home-manager.users.${username} = {
+    stylix = {
+      enable = true;
+      image = ./wallpaper/wallpaper.jpg;
+      cursor = {
+        name = "Adwaita";
+        package = pkgs.gnome.adwaita-icon-theme;
+        size = 24;
+      };
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/classic-dark.yaml";
     };
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/classic-dark.yaml";*/
-  };
-  home-manager.users.${username}.stylix = {
-    enable = true;
-    image = ./wallpaper/wallpaper.jpg;
-    cursor = {
-      name = "Adwaita";
-      package = pkgs.gnome.adwaita-icon-theme;
-      size = 24;
-    };
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/classic-dark.yaml";
+    gtk2.enable = true;
+    gtk3.enable = true;
+    gtk4.enable = true;
   };
 }
