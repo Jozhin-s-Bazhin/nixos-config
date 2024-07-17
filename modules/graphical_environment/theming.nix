b9e3f6acd98f1c148217f5efd0258959518707d5
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-07-17 18:35:14 +0200

    [ Wed Jul 17 06:35:14 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/graphical_environment/theming.nix
+++ modules/graphical_environment/theming.nix
@@ -30,7 +30,7 @@
           name = "Whatsapp Emoji";
 	      };
       };
-      base16Scheme = "${pkgs.base16-schemes}/share/themes/windows-10.yaml";
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/windows-nt.yaml";
     };
     gtk = {
       enable = true;
