ca22cea17a8121754488942280136f589dbbe23d
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-07-17 14:27:54 +0200

    [ Wed Jul 17 02:27:54 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/graphical_environment/desktop/lock.nix
+++ modules/graphical_environment/desktop/lock.nix
@@ -43,29 +43,9 @@
 #!/run/current-system/sw/bin/bash -l
 
 # Environment variables
-_zedlmt() { ${pkgs.coreutils}/bin/od -t x1 -w1 -v  | ${pkgs.coreutils}/bin/sed -n '
-    /.* \(..\)$/s//\1/
-    /00/!{H;b};s///
-    x;s/\n/\\x/gp;x;h'
-}
-
-_pidenv() { ${pkgs.procps}/bin/ps -p $1 >/dev/null 2>&1 &&
-        [ -z "''${1#"$psrc"}" ] && . /dev/fd/3 ||
-        ${pkgs.coreutils}/bin/cat <&3 ; unset psrc pcat
-} 3<<STATE
-        $( [ -z "''${1#''${pcat=$psrc}}" ] &&
-        pcat='$(printf %%b "%s")' || pcat="%b"
-        xeq="$(printf '\\x%x' "'=")"
-        for x in $( _zedlmt </proc/$1/environ ) ; do
-        printf "%b=$pcat\n" "''${x%%"$xeq"*}" "''${x#*"$xeq"}"
-        done)
-#END
-STATE
-_pidenv ''${psrc=$(pgrep -u $USER Hyprland)}
-_pidenv $(pgrep -u $USER Hyprland) > /home/roman/locklogfile
-
 export XDG_RUNTIME_DIR="/run/user/$(loginctl list-sessions | /nix/store/a5rvjq2ir4d1wnxwdf4a9zf6hfc6ydsx-gawk-5.2.2/bin/awk 'NR==2 {print $2}')"
 export WAYLAND_DISPLAY="wayland-$(loginctl list-sessions | /nix/store/a5rvjq2ir4d1wnxwdf4a9zf6hfc6ydsx-gawk-5.2.2/bin/awk 'NR==2 {print $1}')"
+export DBUS_SESSION_ADDRESS="unix:path=/run/user/1000/bus"
 
 ${pkgs.gtklock}/bin/gtklock -L "systemd-notify --ready"
       ''}/bin/lockBeforeSleep";
