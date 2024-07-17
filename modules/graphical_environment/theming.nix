94750adb6cb441e768f1f7829a7010b11990f78f
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-07-17 18:15:17 +0200

    [ Wed Jul 17 06:15:17 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/graphical_environment/theming.nix
+++ modules/graphical_environment/theming.nix
@@ -30,27 +30,7 @@
 	  name = "Whatsapp Emoji";
 	};
       };
-      base16Scheme = {
-        system = "base16";
-        variant = "dark";
-        palette = {
-          base00 = "151515";
-          base01 = "202020";
-          base02 = "303030";
-          base03 = "505050";
-          base04 = "B0B0B0";
-          base05 = "D0D0D0";
-          base06 = "E0E0E0";
-          base07 = "F5F5F5";
-          base08 = "AC4142";
-          base09 = "D28445";
-          base0A = "F4BF75";
-          base0B = "90A959";
-          base0C = "75B5AA";
-          base0D = "066CFA";
-          base0E = "AA759F";
-          base0F = "8F5536";
-        };
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/classic-dark.yml"
       };
     };
     gtk = {
