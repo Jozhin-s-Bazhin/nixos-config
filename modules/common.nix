a7ce0de1c3189d87e09d7144d8e59ffe0c623c04
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-07-17 22:49:41 +0200

    [ Wed Jul 17 10:49:41 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/common.nix
+++ modules/common.nix
@@ -89,7 +89,7 @@
     '';
 
     promptInit = ''
-      export PROMPT='%F{blue}%F{white}%K{blue}%n@%m%F{blue}%k%f %1/ '
+      export PROMPT='%F{blue}%F{white}%K{blue}%n@%m%F{blue}%k%f %/ '
     '';
 
     # Aliases
