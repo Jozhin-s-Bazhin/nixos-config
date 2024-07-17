1bc8566cd44f08e776eda4e880b4baa8a77e38f3
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-07-17 18:13:31 +0200

    [ Wed Jul 17 06:13:31 PM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/graphical_environment/theming.nix
+++ modules/graphical_environment/theming.nix
@@ -38,7 +38,7 @@
           base01 = "202020";
           base02 = "303030";
           base03 = "505050";
-          base04 = "066cfa";
+          base04 = "B0B0B0";
           base05 = "D0D0D0";
           base06 = "E0E0E0";
           base07 = "F5F5F5";
