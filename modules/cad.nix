c6110ae9ce6537fbcd3a0bf6683b54e6be6840bc
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-07-18 11:39:24 +0200

    [ Thu Jul 18 11:39:24 AM CEST 2024 ] nixos-rebuild switch --flake /home/roman/nixos-config#framework

--- modules/cad.nix
+++ modules/cad.nix
@@ -1,6 +1,29 @@
 { pkgs, username, ...}:
 {
-  home-manager.users.${username}.home.packages = with pkgs; [ 
-    (freecad.override {python = pkgs.python311;})
+  nixpkgs.overlays = [
+    (final: prev: {
+      freecad = prev.freecad.overrideAttrs (oldAttrs: rec {
+        python = final.python311Packages.python;
+        shiboken2 = final.python311Packages.shiboken2;
+        pyside2 = final.python311Packages.pyside2;
+        matplotlib = final.python311Packages.matplotlib;
+        scipy = final.python311Packages.scipy;
+        pyyaml = final.python311Packages.pyyaml;
+        gitpython = final.python311Packages.gitpython;
+
+        # Ensure that all build inputs are also using Python 3.11 packages
+        buildInputs = oldAttrs.buildInputs ++ [
+          final.python311Packages.python
+          final.python311Packages.shiboken2
+          final.python311Packages.pyside2
+          final.python311Packages.matplotlib
+          final.python311Packages.scipy
+          final.python311Packages.pyyaml
+          final.python311Packages.gitpython
+        ];
+      });
+    })
   ];
+
+  home-manager.users.${username}.home.packages = with pkgs; [ freecad ];
 }
