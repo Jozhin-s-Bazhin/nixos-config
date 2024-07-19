31b5540b8bfcb0d46267310233302d5a3773a60e
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-07-19 13:31:59 +0200

    Clean up packages

--- modules/graphical_environment/programs.nix
+++ modules/graphical_environment/programs.nix
@@ -2,20 +2,11 @@
 
 {
   home-manager.users.${username}.home.packages = with pkgs; [
-    spotube
-    pavucontrol
     nautilus
     bitwarden
     whatsapp-for-linux
     thunderbird
-    
-    # CLI stuff
-    graphicsmagick
-    neofetch 
-    fastfetch
-    lolcat
-    sl
-    cmatrix
-    pipes-rs
+    spotube
+    pavucontrol   
   ];
 }
