ba697ba4b943a8af5a0ae482d83400bc8790fc68
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-07-19 15:14:01 +0200

    Move host config from modules/ to hosts/

--- modules.nix
+++ modules.nix
@@ -3,34 +3,34 @@ modules:
 let
   hasModule = moduleName: builtins.elem moduleName modules;
 in
-      [ ./modules/common.nix ] ++
-      lib.optional (hasModule "server") ./modules/server.nix ++
-      lib.optional (hasModule "desktop") ./modules/pc.nix ++
-      lib.optional (hasModule "amdgpu") ./modules/amdgpu.nix ++
-      lib.optional (hasModule "graphicalEnvironment") ./modules/graphical_environment ++
-      lib.optionals (hasModule "laptop") [
-        ./modules/pc.nix
-        ./modules/laptop.nix
-      ] ++
-      lib.optionals (hasModule "gaming") [
-        ./modules/graphical_environment
-        ./modules/gaming.nix
-      ] ++
-      lib.optionals (hasModule "development") [
-        ./modules/graphical_environment
-        ./modules/development.nix
-      ] ++
-      lib.optionals (hasModule "cad") [
-        ./modules/graphical_environment
-        ./modules/cad.nix
-      ] ++
-      lib.optionals (hasModule "office") [
-        ./modules/graphical_environment
-        ./modules/office.nix
-      ] ++
-      lib.optionals (hasModule "movies") [
-        ./modules/graphical_environment
-        ./modules/movies.nix
-      ] ++
-      [ ./hardware/${name}.nix ] ++
-      [ ./modules/${name}.nix ]
+  [ ./modules/common.nix ] ++
+  lib.optional (hasModule "server") ./modules/server.nix ++
+  lib.optional (hasModule "desktop") ./modules/pc.nix ++
+  lib.optional (hasModule "amdgpu") ./modules/amdgpu.nix ++
+  lib.optional (hasModule "graphicalEnvironment") ./modules/graphical_environment ++
+  lib.optionals (hasModule "laptop") [
+    ./modules/pc.nix
+    ./modules/laptop.nix
+  ] ++
+  lib.optionals (hasModule "gaming") [
+    ./modules/graphical_environment
+    ./modules/gaming.nix
+  ] ++
+  lib.optionals (hasModule "development") [
+    ./modules/graphical_environment
+    ./modules/development.nix
+  ] ++
+  lib.optionals (hasModule "cad") [
+    ./modules/graphical_environment
+    ./modules/cad.nix
+  ] ++
+  lib.optionals (hasModule "office") [
+    ./modules/graphical_environment
+    ./modules/office.nix
+  ] ++
+  lib.optionals (hasModule "movies") [
+    ./modules/graphical_environment
+    ./modules/movies.nix
+  ] ++
+  [ ./hardware/${name}.nix ] ++
+  [ ./hosts/${name}.nix ]
