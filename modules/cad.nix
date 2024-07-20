7ce98e2cd479ff8f17dfcb30206c94dcb7891e6b
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-07-20 23:39:38 +0200

    Add inputs. to fix the config

--- modules/cad.nix
+++ modules/cad.nix
@@ -1,6 +1,6 @@
-{ pkgs, username, ...}:
+{ inputs, pkgs, username, ...}:
 {
-  imports = [ nix-snapd.nixosModules.default ];
+  imports = [ inputs.nix-snapd.nixosModules.default ];
   services.snap.enable = true;
   home-manager.users.${username}.home.packages = with pkgs; [ 
     freecad
