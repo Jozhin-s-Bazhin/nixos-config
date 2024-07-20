4facba1fe5d26a5af12dad28c4742faaa7177e2b
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-07-21 00:30:24 +0200

    Remove snapd

--- modules/cad.nix
+++ modules/cad.nix
@@ -1,7 +1,5 @@
 { inputs, pkgs, username, ...}:
 {
-  imports = [ inputs.nix-snapd.nixosModules.default ];
-  services.snap.enable = true;
   home-manager.users.${username}.home.packages = with pkgs; [ 
     freecad
     bambu-studio
