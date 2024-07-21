b035fbd51374fa72250cecec85fba18cef32e0ae
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-07-21 22:04:26 +0200

    Change brightness step to 3% instead of 5

--- modules/graphical_environment/desktop/keybinds.nix
+++ modules/graphical_environment/desktop/keybinds.nix
@@ -65,8 +65,8 @@ in
         ", XF86AudioLowerVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%-"
 
         # Brightness
-        ", XF86MonBrightnessUp, exec, brightnessctl -s set 5%+"
-        ", XF86MonBrightnessDown, exec, brightnessctl -s set 5%- -n 1"
+        ", XF86MonBrightnessUp, exec, brightnessctl -s set 3%+"
+        ", XF86MonBrightnessDown, exec, brightnessctl -s set 3%- -n 1"
       ];
 
       bindl = [
