49aa9b965b4b8f0ab473bf3f836865869ae2ec21
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-07-23 18:31:43 +0200

    Enable kwallet

--- modules/graphical_environment/desktop/lock.nix
+++ modules/graphical_environment/desktop/lock.nix
@@ -73,6 +73,12 @@ ${pkgs.gtklock}/bin/gtklock -L "bash -c 'sleep 1; systemd-notify --ready'" --dis
   # Polkit
   users.users.${username}.packages = [ pkgs.lxqt.lxqt-policykit ];
 
+  # KWallet
+  security.pam.services.kwallet = {
+    name = "kwallet";
+    enableKwallet = true;
+  };
+
   # Display manager
   services.displayManager.sddm = {
     enable = true;
