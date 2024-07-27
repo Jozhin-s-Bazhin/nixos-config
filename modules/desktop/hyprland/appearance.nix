2a547d20a33244659d51072338d68573cf28953d
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-07-27 17:54:54 +0200

    Make special workspace slide in from bottom.

--- modules/desktop/hyprland/appearance.nix
+++ modules/desktop/hyprland/appearance.nix
@@ -7,8 +7,6 @@
         gaps_in = 2;
         gaps_out = 4;
         border_size = 2;
-        #"col.active_border" = "rgba(${ colors.nixToHex colors.accent }ff)";
-        #"col.inactive_border" = "rgba(${ colors.nixToHex colors.background_darker }ff)";
         layout = "dwindle";
         allow_tearing = true; 
       }; 
@@ -21,23 +19,19 @@
           passes = 2;
           special = true;
         };
-
-        drop_shadow = false;
-        shadow_range = 4;
-        shadow_render_power = 3;
-        #"col.shadow" = "rgba(1a1a1aee)";
       };
 
       animations = {
         enabled = true;
         bezier = [ "myBezier, 0.05, 0.9, 0.1, 1.05" ];
         animation = [ 
-    "windows, 1, 4, myBezier"
-        "windowsOut, 1, 4, default, popin 80%"
-        "border, 1, 5, default"
-        "borderangle, 1, 4, default"
-    "fade, 1, 4, default"
-        "workspaces, 1, 6, default"
+          "windows, 1, 4, myBezier"
+          "windowsOut, 1, 4, default, popin 80%"
+          "border, 1, 5, default"
+          "borderangle, 1, 4, default"
+          "fade, 1, 4, default"
+          "workspaces, 1, 6, default"
+	  "specialWorkspace, 1, 6, default, slidevert"
         ];
       };
 
