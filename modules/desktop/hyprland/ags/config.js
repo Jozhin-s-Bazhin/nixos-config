346e4a4153608e9d03e872f1c478634895497644
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-08-02 17:43:11 +0200

    ags: remove applauncher

--- modules/desktop/hyprland/ags/config.js
+++ modules/desktop/hyprland/ags/config.js
@@ -1,12 +1,10 @@
 import { Bars } from "./bar.js";
 import { NotificationPopups } from "./notifications.js"
-//import { applauncher } from "./applauncher.js";
 
 App.config({
   style: "./style.css",
   windows: [
     Bars(), // This returns a list but it still works somehow
     NotificationPopups(),
-    //applauncher
   ]
 })
