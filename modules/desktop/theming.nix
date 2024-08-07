f013ad50ab5cdadb7945620beecbe5e18cc94825
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-08-07 23:20:04 +0200

    theming.nix: remove unnecessary kernel params

--- modules/desktop/theming.nix
+++ modules/desktop/theming.nix
@@ -48,14 +48,6 @@
     plymouth.enable = true;
     initrd.verbose = true;
     consoleLogLevel = 0;
-    kernelParams = [ 
-      "quiet" 
-      "splash" 
-      "boot.shell_on_fail"
-      "loglevel=3"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
-    ];
+    kernelParams = [ "quiet" "splash" ];
   };
 }
