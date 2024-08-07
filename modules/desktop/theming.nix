c6a2fc6fdb1dc0e6096a49ae9a8dacf9f6b53276
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-08-07 22:58:53 +0200

    theming.nix: try to make plymouth work

--- modules/desktop/theming.nix
+++ modules/desktop/theming.nix
@@ -48,6 +48,14 @@
     plymouth.enable = true;
     initrd.verbose = true;
     consoleLogLevel = 0;
-    kernelParams = [ "quiet" "splash" ];
+    kernelParams = [ 
+      "quiet" 
+      "splash" 
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
   };
 }
