def0ebc6cd079e7f6c505e79fb32669508ea7504
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-08-08 14:04:10 +0200

    hyprland: Switch to version 0.42.0 from flake

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
 
     stylix.url = "github:danth/stylix/";
     ags.url = "github:Aylur/ags";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&ref=9a09eac79b85c846e3a865a9078a3f8ff65a9259";
   };
 
   outputs = inputs@{ nixpkgs, ... }: {
--- modules/desktop/hyprland/hyprland_config.nix
+++ modules/desktop/hyprland/hyprland_config.nix
@@ -14,12 +14,14 @@
 
     wayland.windowManager.hyprland = {
       enable = true;
+      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
       xwayland.enable = true;
     };
   };
   
   programs.hyprland = {
     enable = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
   };
 
   environment.sessionVariables = {
