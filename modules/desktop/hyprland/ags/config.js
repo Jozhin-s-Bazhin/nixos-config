a10022fe139d29d767c322709ad943ef3332b22d
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-08-24 18:39:32 +0200

    ags: add an osd
    note: slider doesn't display properly

--- modules/desktop/hyprland/ags/config.js
+++ modules/desktop/hyprland/ags/config.js
@@ -1,12 +1,15 @@
 import { Bars } from "./bar.js";
 import { NotificationPopups } from "./notifications.js"
 import { Sidebar } from "./sidebar.js"
+import { BrightnessOsd, VolumeOsd } from "./osd.js"
 
 App.config({
   style: "./style.css",
   windows: [
     ...Bars(),
     NotificationPopups(),
-    Sidebar()
+    Sidebar(),
+    BrightnessOsd(),
+    VolumeOsd()
   ]
 })
---
+++ modules/desktop/hyprland/ags/osd.js
@@ -0,0 +1,92 @@
+import brightness from './services/brightness.js';
+const audio = await Service.import('audio')
+
+const DELAY = 2000
+
+export function VolumeOsd() {
+  const icons = {
+    101: "overamplified",
+    67: "high",
+    34: "medium",
+    1: "low",
+    0: "muted",
+  }
+
+  function getIcon() {
+    const icon = audio.speaker.is_muted ? 0 : [101, 67, 34, 1, 0].find(
+      threshold => threshold <= audio.speaker.volume * 100)
+
+    return `audio-volume-${icons[icon]}-symbolic`
+  }
+
+  const window = Widget.Window({
+    name: "VolumeOsd",
+    class_name: "volume_osd",
+    anchor: ["bottom"],
+    child: Widget.Box({
+      children: [
+        Widget.Icon({
+          icon: Utils.watch(getIcon(), audio.speaker, getIcon),
+        }),
+
+        Widget.Slider({
+          on_change: ({ value }) => audio['speaker'].volume = value,
+          value: audio['speaker'].bind('volume'),
+          draw_value: false,
+          hexpand: true
+        })
+      ],
+    }),
+    visible: false
+  })
+
+  let count = 0;
+  Utils.watch(false, audio, "changed", () => {
+    count++
+    window.visible = true
+    Utils.timeout(DELAY, () => {
+      count--
+      if (count === 0) { window.visible = false }
+    })
+  })
+
+  return window
+}
+
+export function BrightnessOsd() {
+  const window = Widget.Window({
+    name: "BrightnessOsd",
+    class_name: "brightness_osd",
+    anchor: ["bottom"],
+    child: Widget.Box({
+      children: [
+        Widget.Icon ({
+          icon: brightness.bind("screen-value").as(b => 
+            b <= 33 ? "display-brightness-symbolic" :
+            b <= 66 ? "display-brightness-symbolic" :
+            "display-brightness-symbolic"
+          )
+        }),
+        Widget.Slider({
+          on_change: self => brightness.screen_value = self.value,
+          value: brightness.bind('screen-value'),
+          draw_value: false,
+          hexpand: true
+        })
+      ],
+    }),
+    visible: false
+  })
+
+  let count = 0;
+  Utils.watch(false, brightness, "changed", () => {
+    count++
+    window.visible = true
+    Utils.timeout(DELAY, () => {
+      count--
+      if (count === 0) { window.visible = false }
+    })
+  })
+
+  return window
+}
