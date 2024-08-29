cf964635b1cec826a4c67301f159ae5feeaf670e
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-08-29 20:02:41 +0200

    framework: remove kernel param

--- hosts/framework.nix
+++ hosts/framework.nix
@@ -9,8 +9,7 @@
   boot.kernelParams = [ 
     "usbcore.autosuspend=60" 
     "resume_offset=53248" 
-    "rtc_cmos.use_acpi_alarm=1"
-  ]; 
+  ];
   swapDevices = [{
     device = "/var/lib/swapfile";
     size = 36*1024;
