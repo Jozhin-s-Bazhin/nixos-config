0582576e715ef0597bfbafddb180e8b0bbb42e3d
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-11-01 20:35:10 +0100

    cad: add gmsh and calculix dependencies to freecad

--- modules/cad.nix
+++ modules/cad.nix
@@ -1,7 +1,13 @@
 { inputs, pkgs, username, ...}:
 {
   home-manager.users.${username}.home.packages = with pkgs; [ 
-    freecad-wayland
+    (freecad-wayland.overrideAttrs (oldAttrs: rec {
+      buildInputs = oldAttrs.buildInputs ++ [
+        pkgs.python312Packages.gmsh
+	pkgs.gmsh
+        pkgs.calculix
+      ];
+    }))
     openscad
     orca-slicer
   ];
