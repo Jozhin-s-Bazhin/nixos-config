2d741e9b8f0f9836c6272987259c880906b5b1f9
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-11-02 15:15:55 +0100

    cad: add fem dependencies to freecad runtime

--- modules/cad.nix
+++ modules/cad.nix
@@ -7,6 +7,11 @@
 	pkgs.gmsh
         pkgs.calculix
       ];
+      postFixup = oldAttrs.postFixup + ''
+        wrapProgram $out/bin/FreeCAD --prefix PATH : ${pkgs.calculix}/bin
+        wrapProgram $out/bin/FreeCAD --prefix PATH : ${pkgs.gmsh}/bin
+        wrapProgram $out/bin/FreeCAD --prefix PATH : ${pkgs.python312Packages.gmsh}/lib/python3.12/site-packages
+      '';
     }))
     openscad
     orca-slicer
