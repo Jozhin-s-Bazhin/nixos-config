8ab8a166e1aaf26f1076a97872b925f10f0293e7
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-11-21 21:12:34 +0100

    hyprland: remove 'sleep 2' from gtklock startup

--- modules/desktop/hyprland/lock.nix
+++ modules/desktop/hyprland/lock.nix
@@ -59,7 +59,7 @@ loginctl_sessions=$(loginctl list-sessions)
 export XDG_RUNTIME_DIR="/run/user/$(echo "$loginctl_sessions" | ${pkgs.gawk}/bin/awk 'NR==2 {print $2}')"
 export DBUS_SESSION_ADDRESS="unix:path=/run/user/$(echo "$loginctl_sessions" | ${pkgs.gawk}/bin/awk 'NR==2 {print $2}')/bus"
 
-${pkgs.gtklock}/bin/gtklock -L "bash -c 'sleep 2; systemd-notify --ready'" --display "wayland-1"
+${pkgs.gtklock}/bin/gtklock -L "systemd-notify --ready" --display "wayland-1"
       ''}/bin/lockBeforeSleep";
     };
   };
