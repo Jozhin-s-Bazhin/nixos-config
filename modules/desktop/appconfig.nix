5e8ce03c1fc72450f600af8b792acc3432de6a3e
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-12-12 13:15:23 +0100

    desktop: switch from zen to librewolf

--- modules/desktop/appconfig.nix
+++ modules/desktop/appconfig.nix
@@ -1,5 +1,14 @@
 { pkgs, username, config, inputs, ... }:
-
+let
+  lock-false = {
+    Value = false;
+    Status = "locked";
+  };
+  lock-true = {
+    Value = true;
+    Status = "locked";
+  };
+in
 {
   home-manager.users.${username} = {
     # Kitty
@@ -15,33 +24,66 @@
       };
     };
 
-    /*programs.chromium = {
+    # Librewolf
+    programs.firefox = {
       enable = true;
-      package = pkgs.brave;
-      extensions = [
-        { id = "dbepggeogbaibhgnhhndojpepiihcmeb"; }  # Vimium
-	{ id = "hmbgmokpddhjjncclckdfnolbhfjnoam"; }  # SearX
-	{ id = "nngceckbapebfimnlniiiahkandclblb"; }  # Bitwarden
-	{ id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; }  # Dark reader
-        { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; }  # Sponsorblock
-        { id = "fdpohaocaechififmbbbbbknoalclacl"; }  # Page screenshot
-      ];
-      commandLineArgs = [
-        "--enable-features=TouchpadOverscrollHistoryNavigation"
-	"--password-store=basic"
-	"--enable-smooth-scrolling"
-      ];
-    };*/
-    home.packages = [ inputs.zen.packages."${pkgs.stdenv.hostPlatform.system}".specific ];
-  }; 
+      package = (pkgs.wrapFirefox (pkgs.librewolf-unwrapped.override { pipewireSupport = true;}) {});
+      policies = {
+        DisplayBookmarksToolbar = "never";
+	DisplayMenuBar = "never";
+	SearchBar = "unified";
+	OfferToSaveLogins = false;
+	PasswordManagerEnabled = false;
 
-  /*# Brave
-  environment.etc."brave/policies/managed/policies.json".text = ''
-    "TorDisabled"=dword:00000001
-    "IPFSEnabled"=dword:00000000
-    "BraveRewardsDisabled"=dword:00000001
-    "BraveWalletDisabled"=dword:00000001
-    "BraveVPNDisabled"=dword:00000001
-    "BraveAIChatEnabled"=dword:00000000
-  '';*/
+	Preferences = {
+	  "widget.gtk.native-context-menus" = lock-true;
+	  "widget.gtk.non-native-titlebar-buttons.enabled" = lock-false;
+          "sidebar.verticalTabs" = lock-true;
+	};
+
+	# Librewolf policies
+	DisableAppUpdate = true;
+	AppUpdateURL = "https://localhost/";
+	OverrideFirstRunPage = "";
+	OverridePostUpdatePage = "";
+	DisableSystemAddonUpdate = true;
+	DisableProfileImport = false;
+	DisableFirefoxStudies = true;
+	DisableTelemetry = true;
+	DisableFeedbackCommands = true;
+	DisableSetDesktopBackground = false;
+	DisableDeveloperTools = false;
+	NoDefaultBookmarks = true;
+	WebsiteFilter = "{\"Block\":[\"https://localhost/*\"],\"Exceptions\":[\"https://localhost/*\"]}";
+
+	# Extensions
+	ExtensionSettings = {
+	  "*".installation_mode = "blocked";
+
+          # Ublock
+	  "uBlock0@raymondhill.net" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # Dark Reader
+	  "addon@darkreader.org" = {
+	    install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+	    installation_mode = "force_installed";
+	  }; 
+
+          # Vimium
+          "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
+	    install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
+	    installation_mode = "force_installed";
+	  };
+
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+	    install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden/latest.xpi";
+	    installation_mode = "force_installed";
+	  };
+	};
+      };
+    };
+  }; 
 }
