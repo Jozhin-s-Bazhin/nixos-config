b1e0b5ef18be64eaa768f5610b290799d82e1804
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-12-15 19:27:13 +0100

    cad: try to fix cfdof again

--- modules/cad/freecad.nix
+++ modules/cad/freecad.nix
@@ -4,10 +4,12 @@
     pkgs.python312Packages.gmsh
     pkgs.gmsh
     pkgs.calculix
+    pkgs.scotch
   ];
   postFixup = oldAttrs.postFixup + ''
     wrapProgram $out/bin/FreeCAD --prefix PATH : ${pkgs.calculix}/bin
     wrapProgram $out/bin/FreeCAD --prefix PATH : ${pkgs.gmsh}/bin
     wrapProgram $out/bin/FreeCAD --prefix PATH : ${pkgs.python312Packages.gmsh}/lib/python3.12/site-packages
+    wrapProgram $out/bin/FreeCAD --prefix LD_LIBRARY_PATH : ${pkgs.scotch}/lib
   '';
 }))
