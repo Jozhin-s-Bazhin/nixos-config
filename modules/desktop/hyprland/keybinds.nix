cecabcf55d3f88edf9311300d2c647096c0321ec
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-12-18 00:54:52 +0100

    hyprland: fix workspaceSwitcher

--- modules/desktop/hyprland/keybinds.nix
+++ modules/desktop/hyprland/keybinds.nix
@@ -1,54 +1,57 @@
 { config, pkgs, username, ... }:
 let 
 	hyprnome = "${pkgs.hyprnome}/bin/hyprnome";
-	workspaceSwitcher = pkgs.writers.writePython3Bin "workspaceSwitcher" {} ''
-		from subprocess import run
-		from json import loads
-		from sys import argv, exit
+	workspaceSwitcher = pkgs.writers.writePython3Bin "workspaceSwitcher" {
+		flakeIgnore = [ "E265" "E999" "W191" "E117" "W292" "W293" "E101" "E128" ];  # Why is this a thing
+	} 
+	''
+from subprocess import run
+from json import loads
+from sys import argv, exit
 
 
-		def hyprctl_json(message):
-				output = run(f"hyprctl -j {message}", shell=True,
-										 capture_output=True, encoding="utf-8")
-				json = loads(output.stdout)
-				return json
+def hyprctl_json(message):
+		output = run(f"hyprctl -j {message}", shell=True,
+								 capture_output=True, encoding="utf-8")
+		json = loads(output.stdout)
+		return json
 
 
-		def get_workspaces():
-				workspaces = [
-					workspace["id"] for workspace in hyprctl_json("workspaces")
-					if workspace["id"] > 0
-				]
-				workspaces.sort()
-				return workspaces
+def get_workspaces():
+		workspaces = [
+			workspace["id"] for workspace in hyprctl_json("workspaces")
+			if workspace["id"] > 0
+		]
+		workspaces.sort()
+		return workspaces
 
 
-		def get_currentworkspace():
-				monitors = hyprctl_json("monitors")
-				for monitor in monitors:
-						if monitor["focused"]:
-								return monitor["activeWorkspace"]["id"]
+def get_currentworkspace():
+		monitors = hyprctl_json("monitors")
+		for monitor in monitors:
+				if monitor["focused"]:
+						return monitor["activeWorkspace"]["id"]
 
 
-		def get_target_workspace(workspaceid):
-				workspaces = get_workspaces()
+def get_target_workspace(workspaceid):
+		workspaces = get_workspaces()
 
-				workspaceid = int(workspaceid)
-				if workspaceid > len(workspaces):
-						target = workspaces[-1] + 1
-				else:
-						target = workspaces[workspaceid - 1]
-				return target
+		workspaceid = int(workspaceid)
+		if workspaceid > len(workspaces):
+				target = workspaces[-1] + 1
+		else:
+				target = workspaces[workspaceid - 1]
+		return target
 
 
-		target = get_target_workspace(argv[2])
-		if target == "No target workspace":
-				exit()
+target = get_target_workspace(argv[2])
+if target == "No target workspace":
+		exit()
 
-		if argv[1] == "workspace":
-				run(["hyprctl", "dispatch", "workspace", f"{target}"])
-		elif argv[1] == "movetoworkspace":
-				run(["hyprctl", "dispatch", "movetoworkspace", f"{target}"])
+if argv[1] == "workspace":
+		run(["hyprctl", "dispatch", "workspace", f"{target}"])
+elif argv[1] == "movetoworkspace":
+		run(["hyprctl", "dispatch", "movetoworkspace", f"{target}"])
 	'';
 
 	# Workspace bindings
@@ -120,15 +123,15 @@ in
 				''}/bin/gamemode.sh"
 
 				# Disable workspace swipe 
-	"SUPER, F2, exec, ${pkgs.writers.writeBashBin "disableWorkspaceSwipe.sh" ''
-		swipe_on=$(hyprctl getoption gestures:workspace_swipe | awk -F': ' '/: [01]$/ {print $2}')
+				"SUPER, F2, exec, ${pkgs.writers.writeBashBin "disableWorkspaceSwipe.sh" ''
+					swipe_on=$(hyprctl getoption gestures:workspace_swipe | awk -F': ' '/: [01]$/ {print $2}')
 
-		if [ $swipe_on -eq 0 ]; then
-			hyprctl keyword gestures:workspace_swipe true
-		else
-			hyprctl keyword gestures:workspace_swipe false
-		fi
-	''}/bin/disableWorkspaceSwipe.sh"
+					if [ $swipe_on -eq 0 ]; then
+						hyprctl keyword gestures:workspace_swipe true
+					else
+						hyprctl keyword gestures:workspace_swipe false
+					fi
+				''}/bin/disableWorkspaceSwipe.sh"
 			]
 			++
 			workspaceBindings;
