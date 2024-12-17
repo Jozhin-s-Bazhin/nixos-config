fdbdb21f87cd75ba5cf542c0daa7c39cd6779d1d
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-12-17 23:41:17 +0100

    hyprland: add hyprspace plugin

--- modules/desktop/hyprland/plugins.nix
+++ modules/desktop/hyprland/plugins.nix
@@ -1,9 +1,19 @@
-{ inputs, username, ... }:
+{ inputs, pkgs, username, ... }:
 {
 	home-manager.users.${username}.wayland.windowManager.hyprland = {
 		plugins = [
+			pkgs.hyprlandPlugins.hyprspace
 		];
 
-		settings.plugin = {};
+		settings.plugin = {
+			overview = {
+				dragAlpha = 1;
+				panelHeight = 200;
+				hideTopLayers = true;
+				workspaceActiveBorder = "rgba(00000000)";
+				workspaceInactiveBorder = "rgba(00000000)";
+				showNewWorkspace = false;
+			};
+		};
 	};
 }
