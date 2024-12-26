ce06d21a65d5c1f9a86416b6c3360551e05e42e8
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-12-26 10:18:15 +0100

    modules: Add description to options

--- modules/default.nix
+++ modules/default.nix
@@ -18,10 +18,12 @@
     username = lib.mkOption {
       default = "user";
       type = lib.types.str;
+			description = "Username of the user most of the config applies to.";
     };
     configDir = lib.mkOption {
-      default = "/home/${config.nixos-config.username}/nixos-config";
+      default = "/home/${config.nixos-config.username}/nixos-config/";
       type = lib.types.path;
+			description = "Path to your nixos config. Must have a / at the end.";
     }; 
   };
 }
