c288c975838abfe23ae00763dba1de97da992ca2
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-12-29 17:38:25 +0100

    hyprland: restore xwaylandvideobridge window rules
    They are here so I can temporarily install it and still use it without
    the tray icon always being present.

--- modules/desktop/hyprland/other.nix
+++ modules/desktop/hyprland/other.nix
@@ -16,12 +16,12 @@
       ];
 
       windowrulev2 = [
-        /*# XWaylandvideobridge
+        # XWaylandvideobridge
         "opacity 0.0 override 0.0 override,class:^(xwaylandvideobridge)$"
         "noanim,class:^(xwaylandvideobridge)$"
         "noinitialfocus,class:^(xwaylandvideobridge)$"
         "maxsize 1 1,class:^(xwaylandvideobridge)$"
-        "noblur,class:^(xwaylandvideobridge)$"*/
+        "noblur,class:^(xwaylandvideobridge)$"
 
         # LibreOffice
         "suppressevent maximize,class:^libreoffice-*"
