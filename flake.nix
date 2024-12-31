67c2b64959042dc9caa8073919eff9a2f189a05b
Roman Bezroutchko <rbezroutchko@gmail.com>
2024-12-31 16:12:09 +0100

    flake.nix: remove duplicate nixpkgs inputs

--- flake.nix
+++ flake.nix
@@ -3,15 +3,24 @@
 
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs";
+		nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 		home-manager = {
 			url = "github:nix-community/home-manager";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
-		nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
-		stylix.url = "github:danth/stylix/";
-		ags.url = "github:Aylur/ags/67b0e31ded361934d78bddcfc01f8c3fcf781aad";
-		zen.url = "github:youwen5/zen-browser-flake";
+		stylix = {
+			url = "github:danth/stylix/";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+		ags = {
+			url = "github:Aylur/ags/67b0e31ded361934d78bddcfc01f8c3fcf781aad";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+		zen = {
+			url = "github:youwen5/zen-browser-flake";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 	};
 
 	outputs = inputs@{ nixpkgs, ... }: {
@@ -25,3 +34,4 @@
     };
   };
 }
+
