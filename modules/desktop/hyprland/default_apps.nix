518c748c737baa50be8f69a4b8afd653405ddf45
Roman Bezroutchko <rbezroutchko@gmail.com>
2025-01-08 21:26:28 +0100

    hyprland: fix walker
    Change builtins.applications.actions to
    builtins.applications.actions.enabled.

--- modules/desktop/hyprland/default_apps.nix
+++ modules/desktop/hyprland/default_apps.nix
@@ -46,7 +46,7 @@
         "walker/config.json".text = builtins.toJSON {
           "builtins" = {
             "applications" = {
-              "actions" = false;
+              "actions"."enabled" = false;
               "weight" = 6;
             };
             "clipboard" = {
