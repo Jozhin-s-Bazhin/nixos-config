c30dd006b0ba06e7a49fde346d26b7c8bc7029c7
Roman Bezroutchko <rbezroutchko@gmail.com>
2025-02-19 19:04:44 +0100

    Revert "Pin stylix to release-24.11"
    This reverts commit 0b2463d5533042ea5316d0b375281bc6cab842b2.

--- flake.nix
+++ flake.nix
@@ -4,13 +4,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     stylix = {
-      url = "github:danth/stylix/release-24.11";
+      url = "github:danth/stylix/";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     ags = {
