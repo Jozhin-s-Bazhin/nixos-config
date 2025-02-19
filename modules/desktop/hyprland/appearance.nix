d2b02e983f54f713a88424c6cf5d4cc9f1925d38
Roman Bezroutchko <rbezroutchko@gmail.com>
2025-02-19 17:37:39 +0100

    Revert "hyprland: switch to hyprscroller"
    This reverts commit 9a8f5e53bf801841c96b290bff1e6bb4f3c217b2.

--- modules/desktop/hyprland/appearance.nix
+++ modules/desktop/hyprland/appearance.nix
@@ -13,7 +13,7 @@
           gaps_in = 2;
           gaps_out = 4;
           border_size = 2;
-          layout = "scroller";
+          layout = "dwindle";
         };
 
         decoration = {
@@ -47,7 +47,7 @@
           special_scale_factor = 1;
         };
 
-        /*workspace = [
+        workspace = [
           "w[tv1], gapsout:0, gapsin:0"
           "f[1], gapsout:0, gapsin:0"
         ];
@@ -57,7 +57,7 @@
           "rounding 0, floating:0, onworkspace:w[tv1]"
           "bordersize 0, floating:0, onworkspace:f[1]"
           "rounding 0, floating:0, onworkspace:f[1]"
-        ];*/
+        ];
 
         misc = {
           disable_hyprland_logo = true;
--- modules/desktop/hyprland/plugins.nix
+++ modules/desktop/hyprland/plugins.nix
@@ -9,28 +9,19 @@
   config = lib.mkIf config.nixos-config.desktop.hyprland.enable {
     home-manager.users.${config.nixos-config.username}.wayland.windowManager.hyprland = {
       plugins = [
-        #pkgs.hyprlandPlugins.hyprspace
-        pkgs.hyprlandPlugins.hyprscroller
+        pkgs.hyprlandPlugins.hyprspace
       ];
 
       settings.plugin = {
-        /*
-          overview = {
-            dragAlpha = 1;
-            panelHeight = 200;
-            hideTopLayers = true;
-            workspaceActiveBorder = "rgba(00000000)";
-            workspaceInactiveBorder = "rgba(00000000)";
-            #showNewWorkspace = false;
-            showEmptyWorkspace = false;
-            affectStrut = false;
-          };
-        */
-        scroller = {
-          column_default_width = "one";
-          focus_wrap = "false";
-          mode = "row";
-          monitor_options = "(eDP-2 = (mode = row; column_default_width = one; column_widths = one onehalf; window_default_height = one), DP-1 = (mode = row; column_default_width = onethird; window_default_height = one))";
+        overview = {
+          dragAlpha = 1;
+          panelHeight = 200;
+          hideTopLayers = true;
+          workspaceActiveBorder = "rgba(00000000)";
+          workspaceInactiveBorder = "rgba(00000000)";
+          #showNewWorkspace = false;
+          showEmptyWorkspace = false;
+          affectStrut = false;
         };
       };
     };
