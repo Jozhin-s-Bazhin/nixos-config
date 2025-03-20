a2246843504baa8b701140272184ca59405f9b56
Roman Bezroutchko <rbezroutchko@gmail.com>
2025-03-20 21:27:46 +0100

    hyprland: remove multimonitor tweak from ags
    Remove ags script that moves all inactive workspaces from secondary
    monitors to primary monitor since I don't like it anymore.

--- modules/desktop/hyprland/ags/better_multimonitor.js
+++ modules/desktop/hyprland/ags/better_multimonitor.js
@@ -1,38 +1,38 @@
-const hyprland = await Service.import("hyprland")
+const hyprland = await Service.import("hyprland");
 import { Bar } from "./bar.js";
 
-hyprland.monitors.map(monitor => Bar(monitor.id))  // Open bars on all monitors
+hyprland.monitors.map((monitor) => Bar(monitor.id)); // Open bars on all monitors
 
 hyprland.connect("event", (_, name, data) => {
   // Makes workspaces always be on the focused monitor
-  if (name == "focusedmon") {
+  /*if (name == "focusedmon") {
     const monname = data.split(",")[0]  // monname is the name of the newly focused monitor, e.g 'eDP-1'
     const inactive_workspaces = (() => {
       let active_workspaces = []
       for (let monitor of hyprland.monitors) {
         active_workspaces.push(monitor["activeWorkspace"]["id"])
-      }        
+      }
       const all_workspaces = hyprland.workspaces
-      
+
       return all_workspaces
       .filter(workspace => !active_workspaces.includes(workspace.id))
       .map(workspace => workspace.id)
       .filter(id => id >= 0)
     })()
-    
+
     let batch = [];
     for (let workspace of inactive_workspaces) {
       batch.push(`dispatch moveworkspacetomonitor ${workspace} ${monname}`)
     }
     batch = batch.join(" ; ")
-    
+
     const message = `hyprctl --batch '${batch}'`
     Utils.exec(message)
-  }
+  }*/
   // Open new bars when monitors are connected
-  else if (name == "monitoraddedv2" ) {
-    const monitorid = data.split(",")[0]  // We only need monitorid
-    Bar(Number(monitorid))
-    Utils.ExecAsync('bash -c "killall wluma; wluma"')
+  if (name == "monitoraddedv2") {
+    const monitorid = data.split(",")[0]; // We only need monitorid
+    Bar(Number(monitorid));
+    Utils.ExecAsync('bash -c "killall wluma; wluma"');
   }
-})
+});
