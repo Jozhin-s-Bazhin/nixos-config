34a33a8f6ab6c9918b800032d418bb94299b6e68
Roman Bezroutchko <rbezroutchko@gmail.com>
2025-03-24 22:52:35 +0100

    development: change default prompt
    Edit the default prompt in a horrendously overengineered way by fetching
    the default prompt file from github and applying a patch to it. I could
    have just pasted and edited it why did I just waste an hour on this.

--- modules/development.nix
+++ modules/development.nix
@@ -5,6 +5,51 @@
   ...
 }:
 
+let
+  file = builtins.fetchurl {
+    url = "https://raw.githubusercontent.com/zed-industries/zed/main/assets/prompts/content_prompt.hbs";
+    sha256 = "17fc52is4zial3664sqglwkbwn8cmh0nfahpdfjr17x9xlsf3b46";
+  };
+  patch = pkgs.writeText "patch" ''
+    @@ -29,11 +29,8 @@ Generate {{content_type}} based on the following prompt:
+
+     Match the indentation in the original file in the inserted {{content_type}}, don't include any indentation on blank lines.
+
+    -Immediately start with the following format with no remarks:
+    +Immediately start your response with no remarks before nor after, only the code to insert.
+
+    -```
+    -\{{INSERTED_CODE}}
+    -```
+     {{else}}
+     Edit the section of {{content_type}} in <rewrite_this></rewrite_this> tags based on the following prompt:
+
+    @@ -67,9 +64,6 @@ Only make changes that are necessary to fulfill the prompt, leave everything els
+
+     Start at the indentation level in the original file in the rewritten {{content_type}}. Don't stop until you've rewritten the entire section, even if you have no more changes to make, always write out the whole section with no unnecessary elisions.
+
+    -Immediately start with the following format with no remarks:
+    +Immediately start your response with no remarks before nor after, only the rewritten code.
+
+    -```
+    -\{{REWRITTEN_CODE}}
+    -```
+     {{/if}}
+  '';
+  package = pkgs.stdenv.mkDerivation {
+    name = "patchedFile";
+    dontUnpack = true;
+    buildPhase = ''
+      cp ${file} patchedFile
+      patch patchedFile ${patch}
+    '';
+    installPhase = ''
+      mkdir -p $out
+      cp patchedFile $out/patchedFile
+    '';
+  };
+  promptFile = "${package}/patchedFile";
+in
 {
   options.nixos-config.development.enable = lib.mkEnableOption "and configures an IDE and adds bunch of shortcuts and packages";
 
@@ -122,6 +167,7 @@
           }
         ];
       };
+      xdg.configFile."zed/prompt_overrides/content_prompt.hbs".source = promptFile;
     };
     services.ollama = {
       enable = true;
