a5bb0c870cda73bfa6c346348d7ebcc6bd59adec
Roman Bezroutchko <rbezroutchko@gmail.com>
2025-04-03 22:17:05 +0200

    hyprland: fix ags freezing
    The act of importing sidebar.js seems to anger the ags gods and causes
    the process to hang and do nothing. Hopefully this also fixes ags
    occasionally freezing.

--- modules/desktop/hyprland/ags/config.js
+++ modules/desktop/hyprland/ags/config.js
@@ -1,15 +1,15 @@
-import { NotificationPopups } from "./notifications.js"
-import { Sidebar } from "./sidebar.js"
-import { VolumeOsd } from "./osd.js"
-import "./better_multimonitor.js"
+import { NotificationPopups } from "./notifications.js";
+//import { Sidebar } from "./sidebar.js";
+import { VolumeOsd } from "./osd.js";
+import "./better_multimonitor.js";
 
 App.config({
   style: "./style.css",
   icons: "./assets",
   windows: [
     NotificationPopups(),
-    Sidebar(),
+    //Sidebar(),
     //BrightnessOsd(),
-    VolumeOsd()
-  ]
-})
+    VolumeOsd(),
+  ],
+});
