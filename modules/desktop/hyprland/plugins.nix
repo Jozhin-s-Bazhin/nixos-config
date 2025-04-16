29c5bf78a1e00808610442d8dd2a92ffa7015b01
Roman Bezroutchko <rbezroutchko@gmail.com>
2025-04-16 20:14:16 +0200

    hyprland: set up hyprtasking

--- modules/desktop/hyprland/plugins.nix
+++ modules/desktop/hyprland/plugins.nix
@@ -9,35 +9,34 @@
   config = lib.mkIf config.nixos-config.desktop.hyprland.enable {
     home-manager.users.${config.nixos-config.username}.wayland.windowManager.hyprland = {
       plugins = [
-        #pkgs.hyprlandPlugins.hyprspace
-        #inputs.hyprtasking.packages.${pkgs.system}.hyprtasking
+        inputs.hyprtasking.packages.${pkgs.system}.hyprtasking
       ];
 
       settings.plugin = {
         hyprtasking = {
-          layout = "linear";
+          layout = "grid";
 
-          gap_size = 20;
+          gap_size = 10;
           bg_color = "0x00000000";
-          border_size = 2;
-          exit_behavior = [
-            "active"
-            "interacted"
-            "original"
-            "hovered"
-          ];
+          border_size = 0;
+          exit_behavior = "active interacted original";
 
           gestures = {
             enabled = true;
             open_fingers = 3;
-            open_distance = 300;
+            open_distance = 200;
             open_positive = true;
           };
 
+          grid = {
+            rows = 3;
+            columns = 3;
+          };
+
           linear = {
-            height = 400;
+            height = 200;
             scroll_speed = 1.1;
-            blur = 1;
+            blur = 0;
           };
         };
       };
