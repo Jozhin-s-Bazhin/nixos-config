a550ec1df21b5c66e1090ea352f1627cba075277
Roman Bezroutchko <rbezroutchko@gmail.com>
2025-04-19 13:21:15 +0200

    Revert "cosmic: enable hyprland"
    This reverts commit 201413382b16f429afbe4c164fa02b30bc374c12.

--- modules/desktop/cosmic/default.nix
+++ modules/desktop/cosmic/default.nix
@@ -10,14 +10,11 @@
   options.nixos-config.desktop.cosmic.enable = lib.mkEnableOption "the COSMIC desktop";
 
   config = lib.mkIf config.nixos-config.desktop.cosmic.enable {
-    nixos-config.desktop.hyprland.enable = true;
     nix.settings = {
       substituters = [ "https://cosmic.cachix.org/" ];
       trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
     };
-    /*
-      services.desktopManager.cosmic.enable = true;
-      services.displayManager.cosmic-greeter.enable = true;
-    */
+    services.desktopManager.cosmic.enable = true;
+    services.displayManager.cosmic-greeter.enable = true;
   };
 }
