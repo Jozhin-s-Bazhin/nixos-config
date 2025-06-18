df73aae977fa4662b00dc6f537029e25d6965583
Roman Bezroutchko <rbezroutchko@gmail.com>
2025-06-18 09:08:31 +0200

    framework: switch to cosmic
    Man I just want tf2 to work why does it gotta be such a piece of shit

--- hosts/framework.nix
+++ hosts/framework.nix
@@ -34,7 +34,7 @@
     amdgpu.enable = true;
     desktop = {
       enable = true;
-      desktop = "hyprland";
+      desktop = "cosmic";
     };
     development.enable = true;
     gaming.enable = true;
