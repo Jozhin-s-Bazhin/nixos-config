ce65c9138b784e4e931173ec9db08225b19d9f03
Roman Bezroutchko <rbezroutchko@gmail.com>
2025-06-21 18:15:34 +0200

    plasma: add some default apps

--- modules/desktop/plasma/default.nix
+++ modules/desktop/plasma/default.nix
@@ -18,5 +18,10 @@
     programs.kdeconnect.enable = true;
     nixos-config.desktop.theming.enable = false;
     fonts.fonts = [ pkgs.nerd-fonts.symbols-only ];
+    home-manager.users.${config.nixos-config.username}.home.packages = with pkgs.kdePackages; [
+      kcalc
+      merkuro
+      plasma-vault
+    ];
   };
 }
