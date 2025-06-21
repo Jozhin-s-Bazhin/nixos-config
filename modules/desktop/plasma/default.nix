a0b346a52f5067ac1784dceacca14c88359ac6df
Roman Bezroutchko <rbezroutchko@gmail.com>
2025-06-21 18:16:41 +0200

    plasma: use fonts.packages instead of fonts.fonts

--- modules/desktop/plasma/default.nix
+++ modules/desktop/plasma/default.nix
@@ -17,7 +17,7 @@
     };
     programs.kdeconnect.enable = true;
     nixos-config.desktop.theming.enable = false;
-    fonts.fonts = [ pkgs.nerd-fonts.symbols-only ];
+    fonts.packages = [ pkgs.nerd-fonts.symbols-only ];
     home-manager.users.${config.nixos-config.username}.home.packages = with pkgs.kdePackages; [
       kcalc
       merkuro
